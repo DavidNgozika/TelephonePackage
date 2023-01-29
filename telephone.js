0a02e4ea692868ead03a2ff4e30f1ffbead30af1
David Ngozika <davidcngozika@gmail.com>
2023-01-29 22:03:23 +0100

    initial commit

---
+++ telephone.js
@@ -0,0 +1,21 @@
+
+class Telephone {
+    constructor() {
+        this.contacts = [];
+    }
+
+    addPhoneNumber = val => {
+        this.contacts.push(val);
+    }
+
+    removePhoneNumber = () => {
+        for (let i = 0; i < this.contacts.length; i++) {
+
+        }
+    }
+
+    dialPhoneNumber = () => {}
+
+
+}
+
